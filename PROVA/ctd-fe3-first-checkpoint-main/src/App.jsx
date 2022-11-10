8056e11a86c720602323cce208593fbb3f061aad
crodsil <cristiane.rodriguesdasilva@gmail.com>
2022-11-09 22:36:23 -0300

    ajustes-function

--- PROVA/ctd-fe3-first-checkpoint-main/src/App.jsx
+++ PROVA/ctd-fe3-first-checkpoint-main/src/App.jsx
@@ -9,21 +9,49 @@ import ReactDOM from 'react-dom/client';
   function App() {
     const [name, setName] = useState("");
     const [cor, setCor] = useState("");
+    const [allColors, setAllColors] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const shoot = () => {
       alert("Cor favorita adicionada!");
     }
+
+    function insertColor(event) {
+      event.preventDefault()
+      if(inputIsValid()){
+        let color = {
+          name: name.trim(),
+          color: cor.trim()
+        }
+        setAllColors([...allColors, color])
+        setErrorMessage("")
+      } else {
+        setErrorMessage("Adicione corretamente os dados do formulário!")
+      }
+    }
+
+    function inputIsValid(){
+      var color = cor.trim()
+      var name = name.trim()
+      var isValid = false
   
-    return (
+      if(color[0] === ("#") && color.length > 6 && name.length > 3){
+        isValid = true
+      }
+      return isValid
+    }
+  
+     return (
 
       <>
       <div class="borderapp">
         <div class="App">
             <h1 class="title">ADICIONAR NOVA COR</h1>
           <div class="grid-container">
-            <form>
+            <form onSubmit={ event => insertColor(event)}>
               <label class="labelsty">Nome:
                 <input 
+                id="name"
                 type="text" 
                 value={name} 
                 onChange={(e) => setName(e.target.value)}
@@ -33,6 +61,7 @@ import ReactDOM from 'react-dom/client';
             <form>
               <label class="labelsty">Cor:
                 <input 
+                id="color"
                 type="text" 
                 value={cor} 
                 onChange={(e) => setCor(e.target.value)}
@@ -42,6 +71,7 @@ import ReactDOM from 'react-dom/client';
           </div>
         </div>
         <div><button type="submit" onClick={shoot} class="button">Adicionar</button></div>
+        <p>{errorMessage}</p>
       </div>
 
       <Card />
