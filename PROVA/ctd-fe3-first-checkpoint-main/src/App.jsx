dea8c3447df48cd686ecfe674937c5d47c46961c
crodsil <cristiane.rodriguesdasilva@gmail.com>
2022-11-11 20:54:09 -0300

    refatoracao

--- PROVA/ctd-fe3-first-checkpoint-main/src/App.jsx
+++ PROVA/ctd-fe3-first-checkpoint-main/src/App.jsx
@@ -1,86 +1,91 @@
-
-// Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+import { useState } from 'react'
+import './App.css'
 import { Card } from './Card'
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-//function App() {
-  // Aqui você irá criar os Estados para manipular os Inputs
 
-  function App() {
-    const [name, setName] = useState("");
-    const [cor, setCor] = useState("");
-    const [allColors, setAllColors] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
+function App() {
+  const [validators, setValidators] = useState(false)
+  const [cards, setCards] = useState([])
 
-    const shoot = () => {
-      alert("Cor favorita adicionada!");
-    }
-
-    function insertColor(event) {
-      event.preventDefault()
-      if(inputIsValid()){
-        let color = {
-          name: name.trim(),
-          color: cor.trim()
-        }
-        setAllColors([...allColors, color])
-        setErrorMessage("")
-      } else {
-        setErrorMessage("Adicione corretamente os dados do formulário!")
-      }
-    }
-
-    function inputIsValid(){
-      var color = cor.trim()
-      var name = name.trim()
-      var isValid = false
-  
-      if(color[0] === ("#") && color.length > 6 && name.length > 3){
-        isValid = true
-      }
-      return isValid
-    }
-  
-     return (
-
-      <>
-      <div class="borderapp">
-        <div class="App">
-            <h1 class="title">ADICIONAR NOVA COR</h1>
-          <div class="grid-container">
-            <form onSubmit={ event => insertColor(event)}>
-              <label class="labelsty">Nome:
-                <input 
-                id="name"
-                type="text" 
-                value={name} 
-                onChange={(e) => setName(e.target.value)}
-                class="campo" />
-              </label>
-            </form>
-            <form>
-              <label class="labelsty">Cor:
-                <input 
-                id="color"
-                type="text" 
-                value={cor} 
-                onChange={(e) => setCor(e.target.value)}
-                class="campo2" />
-              </label>
-            </form>
-          </div>
-        </div>
-        <div><button type="submit" onClick={shoot} class="button">Adicionar</button></div>
-        <p>{errorMessage}</p>
-      </div>
-
-      <Card />
-    
-      </>    
-    )
+  const shoot = () => {
+    alert("Cor favorita adicionada!");
   }
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App />);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { nameInput, colorInput } = e.target.elements
+    const data = {
+      name: nameInput.value.trim(),
+      color: colorInput.value,
+    }
+
+    const containsNumber = (str) => {
+      return /\d/.test(str);
+    }
+
+    if (data.name.length < 3 || data.color.length < 6 || !containsNumber(data.color)) {
+      setValidators(true)
+    } else {
+      setValidators(false)
+      setCards([...cards, data])
+    }
+  }
+
+  const handleChange = () => {
+    setValidators(false)
+  }
+
+  return (
+    <div className="App">
+      <div className='gridcontent'>
+        <form onSubmit={handleSubmit} className={validators ? 'validators' : ''}>
+          <h1>ADICIONAR NOVA COR</h1>
+
+          <div className='gridContainer'>
+            <div className='inputDiv'>
+              <label htmlFor='nameInput'>Nome:</label>
+              <input
+                id='nameInput'
+                className={validators ? 'validatorsInput' : ''}
+                type='text'
+                placeholder="inserir nome da cor"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='inputDiv'>
+              <label htmlFor='colorInput'>Cor:</label>
+              <input
+                id='colorInput'
+                className={validators ? 'validatorsInput' : ''}
+                type='text'
+                placeholder="inserir cor favorita no formato Hexadecimal"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <button type="submit" disabled={validators} onClick={shoot}>ADICIONAR</button>
+        </form>
+
+        {validators && 
+          <span className={validators ? 'validatorsSpan' : ''}>
+            Adicione corretamente os dados do formulário!
+          </span>
+        }
+
+        <h1>CORES FAVORITAS</h1>
+        <div className='insertColors'>
+          {cards.map(
+            card => {
+              return(
+                <Card key={card.color} colorName={card.name} colorHex={card.color} />
+              )
+            }
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default App
